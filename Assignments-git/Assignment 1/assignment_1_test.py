7962eb466c8cb94bf0964c0863ee6a785434b5de
JohnSmith72 <chris.sc.lam29@gmail.com>
2019-03-28 03:21:39 +1100

    planned ass1

--- Assignments-git/Assignment 1/assignment_1_test.py
+++ Assignments-git/Assignment 1/assignment_1_test.py
@@ -24,9 +24,9 @@ def punctionation_split (file, text):
 		line = line.replace('!', '.')
 		line = line.replace('\n', ' ')
 		line = line.replace(',', ' ') # <----------------- NOTE this is only ok if ',' has no special difference in speech comprehension 
-		print (line)
+		print(line)
 		line_list = line.split(' ')
-		print (line_list)
+		print(line_list)
 		for word in line_list:
 			split = re.search('([\"\'.?!,]*)([a-zA-Z\']*)([\"\'.?!,]*)', word) # word potentially surrounded by punctuation on either side
 			word_split = split.groups()
@@ -37,51 +37,104 @@ def punctionation_split (file, text):
 			text.remove('')
 		except:
 			break
-	return text
 
 # -------------------------------------------------------------- find_sirs fn ------------------------------------------------------------
 # Used to find all sirs, also useful for finding sirs in speech
 	# Not sure if speech sir search is same implementation though
+# Right now, all it does is return the list of sirs
 
 def find_sirs (text, list_of_sirs):
 	word_pos = 0
-	print (len(text))
 	while word_pos != len(text): # Allows situational double jumps
-		print (word_pos)
 		if text[word_pos] == 'Sir':
-			if text[word_pos] not in list_of_sirs:
+			if text[word_pos + 1] not in list_of_sirs:
 				list_of_sirs.append(text[word_pos + 1])
-				print (list_of_sirs)
 		elif text[word_pos] == 'Sirs':
 			while text[word_pos + 1] != 'and':
 				if text[word_pos + 1] not in list_of_sirs:
 					list_of_sirs.append(text[word_pos + 1])
-					print (list_of_sirs)
 				word_pos += 1
+			word_pos += 1
+			if text[word_pos + 1] not in list_of_sirs:
+				list_of_sirs.append(text[word_pos + 1])
 		word_pos += 1
-	list_of_sirs = sorted(list_of_sirs)
-			
+	return sorted(list_of_sirs)
+
+# -------------------------------------------------------------- speech_processor fn ------------------------------------------------------------
+# Used to find all sirs, also useful for finding sirs in speech
+
+def speech_processor (text, dictionary):
+	pass
+
+# -------------------------------------------------------------- truth_processor fn ------------------------------------------------------------
+# Used to find all sirs, also useful for finding sirs in speech
+
+def truth_processor (dictionary_of_statements):
+	pass
 
 # ------------------------------------------------------------------- CODE BODY -----------------------------------------------------------------
 
-file_name = input('Which text file do you want to use for the puzzle? ')
-
-list_of_sirs = [] # Since lists are ordered, I prefer to use lists. Just check for duplication before adding an element
 text = ['.'] # dont have to adjust parsing or sentence checking. Can also remove later
-# special_characters = ('\"', '\'', '.', '?', '!', ',', '\n')        < ------------------# Add more if necessary then also add to regex
+list_of_sirs = [] # Since lists are ordered, I prefer to use lists. Just check for duplication before adding an element
+dictionary_of_statements = {}
+solutions_list = []
 
-
+file_name = input('Which text file do you want to use for the puzzle? ')
 with open('test_1.txt') as file:
-	# split and replace punction for processing purposes
+
+	# return a list of words and punctuation in text
 	punctionation_split(file, text)
-	# find all sirs
+
+	print()
+	print(text)
+	print()
+	print(' '.join(text))
+	print()
+
+	# find all sirs in a sorted list
 	find_sirs(text, list_of_sirs)
-	print ()
-	print (list_of_sirs)
-	print ()
-	print (text)
-	print ()
-	print (' '.join(text))
+
+
+	# Iterating through list of words in string
+	# Store sentence start '.'
+		# NOTE: I think remove the '.' initialisation in text
+		# sentence_start = 0 would do the same thing and we really only need to keep track of sentence endings '.'
+
+	# Iterate, if " found ,then it is a speech sentence
+	# Iterate until end found
+	# Once end found,
+	# Pass start and end into a function to determine statement type
+		# Within this statement, also pass through find_sirs to find sirs mentioned
+		# DOUBLE CHECK ASSUMPTION THAT ONLY 1 statement type can be in a statement
+		# Also, that only 1 can be in statement (i.e. cant say 'Sir Jack is a Knight and Sir Jill is a Knave')
+	# This function would then append a list of tuples to dictionary of lists
+	speech_processor(text, dictionary_of_statements)
+
+	# Lastly, a truth processing function
+	# Given dictionary of list of tuples, find all matching statements
+	# Matching statements determine solution
+	truth_processor(dictionary_of_statements)
+
+	# Printing who the sirs are here
+	print('The Sirs are: ', end = '')
+	for name in range(len(list_of_sirs)):
+		if name != len(list_of_sirs) - 1:
+			print(list_of_sirs[name], end = ' ')
+		else:
+			print(list_of_sirs[name])
+
+	# Printing the solutions here
+	if len(solutions_list) == 0:
+		print('There is no solution.')
+	elif len(solutions_list) > 1:
+		print('There are', len(solutions_list), 'solutions.')
+	else:
+		print('There is a unique solution:')
+		for sir in len(solutions_list):
+			if solutions_list[sir] == 0:
+				print('Sir', list_of_sirs[sir], 'is a Knave.')
+			else:
+				print('Sir', list_of_sirs[sir], 'is a Knight.')
 # ----------------------------------------------------------------------------------------------------------------------------------------------
 
 
